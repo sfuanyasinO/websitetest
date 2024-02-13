1b1352e1c9591479214b5afb1fe26654cb388a6f
sfuanyasinO <67312587+sfuanyasinO@users.noreply.github.com>
2024-02-14 00:46:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const productCards = document.querySelectorAll('.product-card');
 
+  productCards.forEach(card => {
+    card.addEventListener('mouseover', () => {
+      productCards.forEach(otherCard => {
+        if (otherCard !== card) {
+          otherCard.style.filter = 'blur(8px)';
+        }
+      });
+    });
+
+    card.addEventListener('mouseout', () => {
+      productCards.forEach(otherCard => {
+        if (otherCard !== card) {
+          otherCard.style.filter = 'none';
+        }
+      });
+    });
+  });
+});
